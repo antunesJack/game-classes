287a37ac7fc6d136be96897d4d8f1486304f7949
jackline <jackline_antunes@hotmail.com>
2023-11-23 16:16:06 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,23 @@
+class hero {
+    constructor(nome, idade, tipo, ataque){
+        this.name = nome
+        this.age = idade
+        this.type = tipo
+        this.atack = ataque
+    }
+    
+    atacar(){
+    	console.log(`O ${this.type} ${this.name} atacou usando ${this.atack}`);
+    }
+}
+
+let heroi1 = new hero ("Gandalf", 150, "mago", "magia");
+let heroi2 = new hero ("Conan", 40, "guerreiro", "espada");
+let heroi3 = new hero ("Dalai", 100, "monge", "artes marciais");
+let heroi4 = new hero ("Hattori", 20, "ninja", "shuriken");
+
+
+heroi1.atacar()
+heroi2.atacar()
+heroi3.atacar()
+heroi4.atacar()
